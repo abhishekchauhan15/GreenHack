90186b221e9cfa7c0e26ac5da90bf9cf4dc34722
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-12-17 17:09:13 +0530

    before Random

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const jsonfile = require('jsonfile')
-const moment =require('monent')
+const moment = require('moment')
+const simpleGit = require('simple-git')
+// const random = require('random')
+
+
+
 const FILE_PATH = './data.json'
 
 //for formatting the date
-const DATE =moment().format();
+const DATE =moment().subtract(5,'d').format('YYYY-MM-DD');
 
 const data = {
     date:DATE
 }
 
-//typeof fiel you want to edit . writeFile(filepath , data you want to write)
-jsonfile.writeFile(FILE_PATH, data )
+//typeof file you want to edit . writeFile(filepath , data you want to write)
+jsonfile.writeFile(FILE_PATH, data, ()=> {
+    
+    //([list of file paths you want to add])
+    simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push();
+    
+})
+
