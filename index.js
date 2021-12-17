d2abc3682a3304cec04d3c58d86f7b6f507046b5
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-12-17 17:25:13 +0530

    after randomization

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const jsonfile = require('jsonfile')
 const moment = require('moment')
 const simpleGit = require('simple-git')
-// const random = require('random')
+const random = require('random')
 
 
 
 const FILE_PATH = './data.json'
 
-//for formatting the date
-const DATE =moment().subtract(5,'d').format('YYYY-MM-DD');
 
-const data = {
-    date:DATE
+
+
+const makeCommit = (n) => {
+
+    if (n === 0)
+        return simpleGit().push()
+    //number of weeks 
+    const x = random.int(0, 54);
+
+    //number of days
+    const y = random.int(0, 6);
+     
+    //for formatting the date
+    const DATE =moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format('YYYY-MM-DD');
+    
+    const data = {
+        date:DATE
+    }
+
+    console.log(DATE);
+    
+    //typeof file you want to edit . writeFile(filepath , data you want to write)
+    jsonfile.writeFile(FILE_PATH, data, ()=> {
+        
+        //([list of file paths you want to add])
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this , --n ));
+        
+    })
+
 }
 
-//typeof file you want to edit . writeFile(filepath , data you want to write)
-jsonfile.writeFile(FILE_PATH, data, ()=> {
-    
-    //([list of file paths you want to add])
-    simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push();
-    
-})
+
+makeCommit(12);
 
